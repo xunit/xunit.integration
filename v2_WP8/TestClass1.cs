519e24e81be3640013e1e8e128f1aab97b67ee70
Oren Novotny <oren@novotny.org>
2014-11-20 12:25:59 -0500

    Added integration test for device runner

---
+++ v2_WP8/TestClass1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace v2_WP8
+{
+    public class TestClass1
+    {
+        [Fact]
+        public void ThisIsATest()
+        {
+            
+        }
+    }
+}
