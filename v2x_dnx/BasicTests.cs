133bef102ec1c2a37883f26691c2a5d54850b68a
Brad Wilson <dotnetguy@gmail.com>
2015-08-23 11:41:52 -0700

    Rename existing v2 projects to v2x, add v20_Win81 and v20_WPA81

---
+++ v2x_dnx/BasicTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace v2x_dnx
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void Passing()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Failing()
+        {
+            Assert.True(false);
+        }
+
+        [Theory]
+        [InlineData(2112)]
+        [InlineData("hello")]
+        [InlineData(null)]
+        public void Theory(object value)
+        {
+            Assert.NotNull(value);
+        }
+    }
+}
---
+++ v2x_net45/DeserializationFailureTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+public class DeserializationFailureTests
+{
+    [CustomFact]
+    public void TestCaseSerializerIsBroken() { }
+
+    [Theory]
+    [MemberData(nameof(BadTheoryData))]
+    public void TheoryDataSerializerIsBroken(BadTheoryItem item) { }
+
+    public static TheoryData<BadTheoryItem> BadTheoryData = new TheoryData<BadTheoryItem> { new BadTheoryItem(42) };
+}
+
+[XunitTestCaseDiscoverer("CustomFactDiscoverer", "v2x_net45")]
+class CustomFactAttribute : FactAttribute { }
+
+class CustomFactDiscoverer : IXunitTestCaseDiscoverer
+{
+    readonly IMessageSink diagnosticMessageSink;
+
+    public CustomFactDiscoverer(IMessageSink diagnosticMessageSink)
+    {
+        this.diagnosticMessageSink = diagnosticMessageSink;
+    }
+
+    public IEnumerable<IXunitTestCase> Discover(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IAttributeInfo factAttribute)
+    {
+        yield return new CustomTestCase(diagnosticMessageSink, discoveryOptions.MethodDisplayOrDefault(), testMethod);
+    }
+}
+
+class CustomTestCase : XunitTestCase
+{
+    // Configuration problem: failed to provide an empty constructor
+
+    public CustomTestCase(IMessageSink diagnosticMessageSink, TestMethodDisplay defaultMethodDisplay, ITestMethod testMethod, object[] testMethodArguments = null)
+        : base(diagnosticMessageSink, defaultMethodDisplay, testMethod, testMethodArguments)
+    { }
+}
+
+public class BadTheoryItem : IXunitSerializable
+{
+    // Configuration problem: failed to provide an empty constructor
+
+    public BadTheoryItem(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; private set; }
+
+    public void Deserialize(IXunitSerializationInfo info)
+    {
+        Value = info.GetValue<int>(nameof(Value));
+    }
+
+    public void Serialize(IXunitSerializationInfo info)
+    {
+        info.AddValue(nameof(Value), Value);
+    }
+}
