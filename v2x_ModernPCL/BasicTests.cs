3df1172d956ad63929ab73905f10ecc30f8444f9
Brad Wilson <dotnetguy@gmail.com>
2015-08-23 18:49:48 -0700

    Add v2x_ModernPCL (doesn't really work anywhere yet)

---
+++ v2x_ModernPCL/BasicTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+public class BasicTests
+{
+    [Fact]
+    public void Passing()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Failing()
+    {
+        Assert.True(false);
+    }
+
+    [Theory]
+    [InlineData(2112)]
+    [InlineData("hello")]
+    [InlineData(null)]
+    public void Theory(object value)
+    {
+        Assert.NotNull(value);
+    }
+
+    [Fact]
+    public async void FailingAsyncVoidTest()
+    {
+        await Task.Delay(0);
+        Assert.True(false);
+    }
+
+    [Fact]
+    public async Task FailingAsyncTaskTest()
+    {
+        await Task.Delay(0);
+        Assert.True(false);
+    }
+}
