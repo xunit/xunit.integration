5fbe046d80d349c392d678256b43551cdc423fb3
gulbanana <castiglione@mac.com>
2016-07-28 07:07:26 +0800

    add an integration test demonstrating that cross-assembly inherited tests have correct source information (#6)

---
+++ v2x_net45/AbstractTestClass.cs
@@ -0,0 +1,7 @@
+﻿using Xunit;
+
+public abstract class AbstractTestClass
+{
+    [Fact]
+    public void TestHasSourceInfo() { } // made concrete by v2x_net45_referencingproject; should not show under v2x_net45, not in the External category
+}
