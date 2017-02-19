d86b369929adddacd034aef491f463eb8a5028bd
Brad Wilson <dotnetguy@gmail.com>
2017-02-19 14:44:40 -0800

    Add v2x_net452_DotNetSdk

---
+++ v2x_net452_DotNetSdk/BasicTests.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+public class BasicTests
+{
+    [Fact]
+    public void Passing()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Failing()
+    {
+        Assert.True(false);
+    }
+
+    [Theory]
+    [InlineData(2112)]
+    [InlineData("hello")]
+    [InlineData(null)]
+    public void Theory(object value)
+    {
+        Assert.NotNull(value);
+    }
+}
