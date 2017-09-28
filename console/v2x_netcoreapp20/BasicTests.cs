e294c7e1b5246aaeb5da9a4cdfad828401b9b0ba
Brad Wilson <dotnetguy@gmail.com>
2017-09-27 19:06:32 -0700

    console: update to 2.3.0-rc1-build3795

---
+++ console/v2x_netcoreapp20/BasicTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+public class BasicTests
+{
+    [Fact]
+    public void Passing()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Failing()
+    {
+        Assert.True(false);
+    }
+
+    [Theory]
+    [InlineData(2112)]
+    [InlineData("hello from v2x_netcoreapp20")]
+    [InlineData(null)]
+    public void Theory(object value)
+    {
+        Assert.NotNull(value);
+    }
+}
