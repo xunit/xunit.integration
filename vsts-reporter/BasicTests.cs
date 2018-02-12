05d8cca86fb0a2ec040f405415a0c1d3f1a4ef57
Oren Novotny <oren@novotny.org>
2018-02-12 09:36:18 -0500

    Add simple passing tests

---
+++ vsts-reporter/BasicTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+public class BasicTests
+{
+    [Fact]
+    public void Passing()
+    {
+        Assert.True(true);
+    }
+
+    [Theory]
+    [InlineData(2112)]
+    [InlineData("hello from vsts-reporter")]
+    public void Theory(object value)
+    {
+        Assert.NotNull(value);
+    }
+}
