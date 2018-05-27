a2934e3fef57fff33bb081d6d156533cc2bac58c
Brad Wilson <dotnetguy@gmail.com>
2018-05-26 18:55:22 -0700

    Add test-explorer/v2x_netcoreapp2.1

---
+++ test-explorer/v2x_netcoreapp2.1/BasicTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+public class BasicTests
+{
+    [Fact]
+    public void Passing()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Failing()
+    {
+        Assert.True(false);
+    }
+
+    [Theory]
+    [InlineData(2112)]
+    [InlineData("hello from v2x_netcoreapp2.1")]
+    [InlineData(null)]
+    [MemberData(nameof(PlatformName))]
+    public void Theory(object value)
+    {
+        Assert.NotNull(value);
+    }
+
+    public static TheoryData<string> PlatformName
+        => new TheoryData<string> { System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription };
+}
