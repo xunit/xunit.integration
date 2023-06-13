887ea575a98397b39b80e1389a444deda27a4f99
Brad Wilson <dotnetguy@gmail.com>
2023-06-13 16:24:08 -0700

    Enable nullable for .NET Core 3.1

--- analyzers/FullXunit/1xxx_Usage/xUnit1015.cs
+++ analyzers/FullXunit/1xxx_Usage/xUnit1015.cs
@@ -1,4 +1,5 @@
-﻿#pragma warning disable xUnit1026 // Theory methods should use all of their parameters
+﻿#pragma warning disable CS8618    // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable xUnit1026 // Theory methods should use all of their parameters
 
 using System.Collections.Generic;
 using Xunit;
--- analyzers/FullXunit/1xxx_Usage/xUnit1018.cs
+++ analyzers/FullXunit/1xxx_Usage/xUnit1018.cs
@@ -1,4 +1,6 @@
-﻿#pragma warning disable xUnit1026 // Theory methods should use all of their parameters
+﻿#pragma warning disable CS0067    // The event 'xUnit1018.TestData' is never used
+#pragma warning disable CS8618    // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable xUnit1026 // Theory methods should use all of their parameters
 
 using System;
 using Xunit;
