e05ee29d854d47bcf68e4afdbcb847e5cf088382
Brad Wilson <dotnetguy@gmail.com>
2023-06-23 16:47:11 -0700

    Add additional example for xUnit1013 to test fixer paths

--- analyzers/v2-full/1xxx_Usage/xUnit1013.cs
+++ analyzers/v2-full/1xxx_Usage/xUnit1013.cs
@@ -5,5 +5,9 @@ public class xUnit1013
     [Fact]
     public void TestMethod1() { }
 
+    // The fixer should offer to convert this to a Fact, because it has no parameters
     public void TestMethod2() { }
+
+    // The fixer should offer to convert this to a Theory, because it has parameters
+    public void TestMethod3(int _) { }
 }
