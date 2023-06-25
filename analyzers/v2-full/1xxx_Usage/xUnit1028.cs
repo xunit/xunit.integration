2061982248ac6f90063b3ba6681a70e2142679c0
Brad Wilson <dotnetguy@gmail.com>
2023-06-25 14:44:28 -0700

    Add xUnit1028 analyzer samples (differ between v2 and v3)

---
+++ analyzers/v2-full/1xxx_Usage/xUnit1028.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+public class xUnit1028
+{
+    [Fact]
+    public int InvalidReturnType()
+    {
+        return 42;
+    }
+
+    // This should trigger, because ValueTask is not supported by v2 (only Task)
+    [Fact]
+    public ValueTask UnsupportedTaskReturnType()
+    {
+        return default;
+    }
+}
---
+++ analyzers/v3-full/1xxx_Usage/xUnit1028.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+public class xUnit1028
+{
+    [Fact]
+    public int InvalidReturnType()
+    {
+        return 42;
+    }
+
+    // This shouldn't trigger, because v3 supports ValueTask
+    [Fact]
+    public ValueTask SupportedTaskReturnType()
+    {
+        return default;
+    }
+}
