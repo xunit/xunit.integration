83ca0d93d186c16dead5cfceb1dbef75799b9a34
Brad Wilson <dotnetguy@gmail.com>
2023-06-26 13:40:51 -0700

    Update the sample for xUnit1019 for ITheoryDataRow

--- analyzers/common/1xxx_Usage/xUnit1019.cs
+++ analyzers/common/1xxx_Usage/xUnit1019.cs
@@ -2,12 +2,24 @@
 
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Sdk;
 
 public class xUnit1019
 {
     public static IEnumerable<object> TestData;
 
+    // For v2, the only fixer offered should be to convert to IEnumerable<object[]>
+    // For v3, it should also offer a fixer to convert to IEnumerable<ITheoryDataRow>
     [Theory]
     [MemberData(nameof(TestData))]
     public void TestMethod(string _1, int _2) { }
+
+#if XUNIT_V3
+    public static List<TheoryDataRow> V3TestData;
+
+    // This should not trigger with v3
+    [Theory]
+    [MemberData(nameof(V3TestData))]
+    public void SkippedDataRow(int _1, string _2) { }
+#endif
 }
