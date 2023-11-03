ba88059b7375de8595ebdd5876c5ba75bc63aaaa
Brad Wilson <dotnetguy@gmail.com>
2023-11-02 22:42:24 -0700

    Add additional examples of xUnit1012 for xunit/xunit.analyzers#164

--- analyzers/common/1xxx_Usage/xUnit1012.cs
+++ analyzers/common/1xxx_Usage/xUnit1012.cs
@@ -2,7 +2,25 @@
 
 public class xUnit1012
 {
+    // nullable struct
     [Theory]
     [InlineData(null)]
-    public void TestMethod(int _) { }
+    public void TestMethod1a(int _) { }
+
+    // nullable reference type
+    [Theory]
+    [InlineData(null)]
+    public void TestMethod1b(string _) { }
+
+#nullable disable
+
+    // nullable struct unaffected by disabling nullable reference types
+    [Theory]
+    [InlineData(null)]
+    public void TestMethod2a(int _) { }
+
+    // reference type won't trigger with nullable reference types disabled
+    [Theory]
+    [InlineData(null)]
+    public void TestMethod2b(string _) { }
 }
