2737d9feff0bdb4ff86e094e3ed45bdd0bb737e7
Brad Wilson <dotnetguy@gmail.com>
2023-12-05 10:44:29 -0800

    Update for xUnit2024 and xUnit2025

---
+++ analyzers/common/2xxx_Assertions/xUnit2024.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+public class xUnit2024
+{
+    [Fact]
+    public void TestMethod()
+    {
+        var x = 42;
+
+        Assert.True(x == 2112);
+    }
+}
---
+++ analyzers/common/2xxx_Assertions/xUnit2025.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+public class xUnit2025
+{
+    [Fact]
+    public void TestMethod()
+    {
+        var x = true;
+
+        Assert.True(x == true);
+    }
+}
