df05418fc3ecdc9facc60bec41572ec96e9c5256
Brad Wilson <dotnetguy@gmail.com>
2023-12-12 16:30:28 -0800

    xunit/xunit#2849 Add additional example for xUnit1030 with ConfigureAwaitOptions

--- analyzers/common/1xxx_Usage/xUnit1030.cs
+++ analyzers/common/1xxx_Usage/xUnit1030.cs
@@ -1,4 +1,5 @@
 #pragma warning disable xUnit1013 // Public method should be marked as test
+#pragma warning disable xUnit1031 // Do not use blocking task operations in test method
 
 using System.Threading.Tasks;
 using Xunit;
@@ -8,14 +9,24 @@ public class xUnit1030
     [Fact]
     public async void TestMethod_Async()
     {
+        var trueValue = true;
+
         await Task.Delay(1).ConfigureAwait(false);
+        await Task.Delay(1).ConfigureAwait(1 == 0);
+        await Task.Delay(1).ConfigureAwait(1 == 1);
+        await Task.Delay(1).ConfigureAwait(trueValue);
     }
 
     [Theory]
     [InlineData(42)]
     public void TestMethod_NonAsync(int delay)
     {
-        Task.Delay(delay).ConfigureAwait(true).GetAwaiter().GetResult();
+        var trueValue = true;
+
+        Task.Delay(delay).ConfigureAwait(false).GetAwaiter().GetResult();
+        Task.Delay(delay).ConfigureAwait(1 == 0).GetAwaiter().GetResult();
+        Task.Delay(delay).ConfigureAwait(1 == 1).GetAwaiter().GetResult();
+        Task.Delay(delay).ConfigureAwait(trueValue).GetAwaiter().GetResult();
     }
 
     public class MyFactAttribute : FactAttribute { }
@@ -30,4 +41,14 @@ public class xUnit1030
     {
         await Task.Delay(1).ConfigureAwait(false);
     }
+
+#if NETCOREAPP
+
+    [Fact]
+    public async void TestMethodWithOptionsEnum()
+    {
+        await Task.Delay(1).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+    }
+
+#endif
 }
