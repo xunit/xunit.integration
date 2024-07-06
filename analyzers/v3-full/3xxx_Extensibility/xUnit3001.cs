0b96f0262f33f1635b0efb148e0f0c0c5e4780f4
Brad Wilson <dotnetguy@gmail.com>
2024-07-05 19:54:06 -0700

    Latest dependencies

---
+++ analyzers/v3-full/3xxx_Extensibility/xUnit3001.cs
@@ -0,0 +1,11 @@
+using Xunit.Sdk;
+
+public class xUnit3001 : IXunitSerializable
+{
+    // Need at least one ctor so the auto-generated ctor doesn't satisfy the rule
+    public xUnit3001(int _) { }
+
+    public void Deserialize(IXunitSerializationInfo info) { }
+
+    public void Serialize(IXunitSerializationInfo info) { }
+}
