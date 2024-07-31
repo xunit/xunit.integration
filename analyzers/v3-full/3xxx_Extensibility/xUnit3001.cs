098292abc59bd47b732a25c8a40a789fe10fca1b
Brad Wilson <dotnetguy@gmail.com>
2024-07-30 17:39:31 -0700

    Latest dependencies

--- analyzers/v3-full/3xxx_Extensibility/xUnit3001.cs
+++ analyzers/v3-full/3xxx_Extensibility/xUnit3001.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit.Sdk;
 
 public class xUnit3001 : IXunitSerializable
@@ -8,4 +9,15 @@ public class xUnit3001 : IXunitSerializable
     public void Deserialize(IXunitSerializationInfo info) { }
 
     public void Serialize(IXunitSerializationInfo info) { }
+}
+
+[JsonTypeID("json-type")]
+public class xUnit3001Json : IJsonSerializable, IJsonDeserializable
+{
+    // Need at least one ctor so the auto-generated ctor doesn't satisfy the rule
+    public xUnit3001Json(int _) { }
+
+    public void FromJson(IReadOnlyDictionary<string, object?> root) { }
+
+    public string ToJson() => "{}";
 }
