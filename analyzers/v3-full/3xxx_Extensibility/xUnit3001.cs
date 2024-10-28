53adbf4cb2c43f625b4656381a99bf6bbb062fc7
Brad Wilson <dotnetguy@gmail.com>
2024-10-27 21:56:41 -0700

    Latest dependencies + updated xUnit3001 samples

--- analyzers/v3-full/3xxx_Extensibility/xUnit3001.cs
+++ analyzers/v3-full/3xxx_Extensibility/xUnit3001.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit.Runner.Common;
 using Xunit.Sdk;
 
 public class xUnit3001 : IXunitSerializable
@@ -19,5 +22,38 @@ public class xUnit3001Json : IJsonSerializable, IJsonDeserializable
 
     public void FromJson(IReadOnlyDictionary<string, object?> root) { }
 
-    public string ToJson() => "{}";
+    public string? ToJson() => "{}";
+}
+
+public class xUnit3001Serializer : IXunitSerializer
+{
+    // Need at least one ctor so the auto-generated ctor doesn't satisfy the rule
+    public xUnit3001Serializer(int _) { }
+
+    public object Deserialize(Type type, string serializedValue) => new();
+
+    public bool IsSerializable(Type type, object? value) => true;
+
+    public string Serialize(object value) => string.Empty;
+}
+
+public class xUnit3001Reporter : IRunnerReporter
+{
+    // Need at least one ctor so the auto-generated ctor doesn't satisfy the rule
+    protected xUnit3001Reporter(int _) { }
+
+    public bool CanBeEnvironmentallyEnabled => false;
+
+    public string Description => string.Empty;
+
+    public bool ForceNoLogo => false;
+
+    public bool IsEnvironmentallyEnabled => false;
+
+    public string? RunnerSwitch => "custom";
+
+    public ValueTask<IRunnerReporterMessageHandler> CreateMessageHandler(
+        IRunnerLogger logger,
+        IMessageSink? diagnosticMessageSink) =>
+            throw new NotImplementedException();
 }
