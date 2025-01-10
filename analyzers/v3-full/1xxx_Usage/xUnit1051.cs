d923e5d811eba35a08fd9b314788ccf00104992c
Brad Wilson <dotnetguy@gmail.com>
2025-01-09 23:23:39 -0800

    Latest dependencies

--- analyzers/v3-full/1xxx_Usage/xUnit1051.cs
+++ analyzers/v3-full/1xxx_Usage/xUnit1051.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -7,5 +8,15 @@ public class xUnit1051
     public async ValueTask TestMethod()
     {
         await Task.Delay(1);
+        await Record.ExceptionAsync(() => Task.Delay(1));
+        await Assert.ThrowsAsync<InvalidOperationException>(() => Task.Delay(1));
+        Assert.Collection(
+            Array.Empty<int>(),
+            x => Task.Delay(1),
+            x => Task.Delay(2)
+        );
+
+        var _ = async () => await Task.Delay(1);
+        Task LocalMethod() => Task.Delay(1);
     }
 }
