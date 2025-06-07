36892d4b227a7fa5b5aa1adbd6ed031b79a87464
Brad Wilson <dotnetguy@gmail.com>
2025-06-06 23:58:19 -0700

    Update xUnit1004 sample to include SkipUnless

--- analyzers/common/1xxx_Usage/xUnit1004.cs
+++ analyzers/common/1xxx_Usage/xUnit1004.cs
@@ -4,4 +4,13 @@ public class xUnit1004
 {
     [Fact(Skip = "This is a flaky test")]
     public void TestMethod() { }
+
+#if XUNIT_V3 && NETCOREAPP
+
+    [Fact(Skip = "Requires Windows",
+          SkipUnless = nameof(System.OperatingSystem.IsWindows),
+          SkipType = typeof(System.OperatingSystem))]
+    public void WindowsTest() { }
+
+#endif
 }
